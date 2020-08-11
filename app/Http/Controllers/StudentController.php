b966abe432c7e77100286ac0db2083b3ae5a2af4
sabreenMohammed107 <sabreenm312@gmail.com>
2020-08-11 17:42:39 +0200

    bb

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -77,71 +77,74 @@ class StudentController extends Controller
         $courses = $request->input('course');
         $randomCoupon = Coupon_data::where('coupon_status', 1)->inRandomOrder()->first();
         $phoneExtist = Coupon_data::with('student')->where('coupon_status', 2)->whereNotNull('student_id')->get();
-    $count=0;
-        foreach( $phoneExtist as  $extist){
+        $count = 0;
+        foreach ($phoneExtist as  $extist) {
 
-        $testing= $extist->student()->get();
-        foreach($testing as $xx){
-            if($xx->mobile==$request->input('mobile'))
-            $count=$count+1;
+            $testing = $extist->student()->get();
+            foreach ($testing as $xx) {
+                if ($xx->mobile == $request->input('mobile'))
+                    $count = $count + 1;
+            }
         }
-       
-    }
-     if($randomCoupon){
-   if($count>0){
-    return redirect()->back()->withInput()->with('flash_success', 'لديك خصم على هذا الرقم!');
-        
-}else{
+        if ($randomCoupon) {
+            if ($count > 0) {
+                return redirect()->back()->withInput()->with('flash_success', 'لديك خصم على هذا الرقم!');
+            } else {
 
 
-        DB::transaction(function () use ($data, $durations, $courses, $randomCoupon, $request) {
-            
-            $student = Student_data::create($data);
-            if ($durations) {
-                foreach ($durations as $dur) {
-                    Preferred_duration::create([
+                DB::transaction(function () use ($data, $durations, $courses, $randomCoupon, $request) {
+
+                    $student = Student_data::create($data);
+                    if ($durations) {
+                        foreach ($durations as $dur) {
+                            Preferred_duration::create([
+                                'student_id' => $student->id,
+                                'duration_id' => $dur,
+                            ]);
+                        }
+                    }
+                    if ($courses) {
+                        foreach ($courses as $course) {
+                            Students_course::create([
+                                'student_id' => $student->id,
+                                'course_id' => $course,
+                            ]);
+                        }
+                    }
+
+                    $coupon = [
                         'student_id' => $student->id,
-                        'duration_id' => $dur,
-                    ]);
-                }
+                        'assign_date' => Carbon::parse(now()),
+                        'coupon_status' => 2,
+
+                    ];
+
+                    $randomCoupon->update($coupon);
+                });
+
+                return view('home.cong', compact('randomCoupon'));
             }
-            if ($courses) {
-                foreach ($courses as $course) {
-                    Students_course::create([
-                        'student_id' => $student->id,
-                        'course_id' => $course,
-                    ]);
-                }
-            }
+        } else {
 
-            $coupon = [
-                'student_id' => $student->id,
-                'assign_date' => Carbon::parse(now()),
-                'coupon_status' => 2,
-
-            ];
-
-            $randomCoupon->update($coupon);
-        });
-
-        return view('home.cong', compact('randomCoupon'));
-    }
-     }else{
-
-        return redirect()->route($this->routeName . 'index')->with('flash_success', 'لايوجد كوبون خصم حاليا !');
-     }
-        
+            return redirect()->route($this->routeName . 'index')->with('flash_success', 'لايوجد كوبون خصم حاليا !');
+        }
     }
     public function search()
     {
-        $coupon=new Coupon_data();
-        return view('home.search' ,compact('coupon'));
+        $coupon = new Coupon_data();
+        return view('home.search', compact('coupon'));
     }
     public function searchResult(Request $request)
     {
         $mobile = $request->input('phone');
-        $student = Student_data::where('mobile', $mobile)->first();
-        $coupon = Coupon_data::where('coupon_status', 2)->where('student_id', $student->id)->first();
+        $students = Student_data::where('mobile', $mobile)->get();
+        foreach ($students as $student) {
+
+            $coupon = Coupon_data::where('coupon_status', 2)->where('student_id', $student->id)->first();
+            if ($coupon) {
+                break;
+            }
+        }
         return view('home.result', compact('coupon'))->render();
     }
     /**
