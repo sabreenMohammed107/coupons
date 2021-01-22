f44c7d2570b9c23bbb2e2b67143dd326d7bff216
sabreenMohammed107 <sabreenm312@gmail.com>
2021-01-22 13:20:20 +0200

    new Coupons

--- database/factories/CouponFactory.php
+++ database/factories/CouponFactory.php
@@ -1,6 +1,7 @@
 <?php
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
+
 use App\Coupon_data;
 use App\Model;
 use Carbon\Carbon;
@@ -8,9 +9,9 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Coupon_data::class, function (Faker $faker) {
     return [
-        'coupon_code' => strtolower(str_random(7)),
-        'discount_per' =>(int)$faker->boolean(60)? 15 : 20,
-        'expired_date' => Carbon::parse('2020-11-25'),
+        'coupon_code' => strtolower(str_random(8)),
+        'discount_per' => (int)$faker->boolean(60) ? 15 : 20,
+        'expired_date' => Carbon::parse('2020-01-25'),
         'coupon_status' => 1,
     ];
 });
