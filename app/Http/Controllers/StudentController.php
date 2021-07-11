34c9fa677006ea9d70c8824e2180f33aa5caf2a1
sabreenMohammed107 <sabreenm312@gmail.com>
2021-07-11 20:51:59 +0200

    add ux

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -37,13 +37,13 @@ class StudentController extends Controller
     public function home()
     {
         $durations = Duration::where('id', '<>', 3)->get();
-        $courses = Course::limit(10)->get();
+        $courses = Course::limit(11)->get();
         return view('home.register', compact('durations', 'courses'));
     }
     public function index()
     {
         $durations = Duration::where('id', '<>', 3)->get();
-        $courses = Course::limit(10)->get();
+        $courses = Course::limit(11)->get();
         return view('home.register', compact('durations', 'courses'));
     }
 
